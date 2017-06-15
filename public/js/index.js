b354c11c5626478cc2affd9f2971a191281282c5
Huzefa <huzefa77@gmail.com>
2017-06-15 19:47:52 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,33 +23,38 @@ socket.on('newLocationMessage', (message) => {
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    jQuery('#messages').append(li);    
+    jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', (e) => {
     e.preventDefault();
+    const messageTextbox = jQuery('[name=message]');
+
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, () => {
-
+        messageTextbox.val('');
     })
 });
 
 const locationButton = jQuery('#send-location');
 locationButton.on('click', () => {
-    if(!navigator.geolocation) {
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         });
     }, () => {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
-
