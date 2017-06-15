e4b4ec37a3a646f6f84838c1c06f84ca6db7a0e4
Huzefa <huzefa77@gmail.com>
2017-06-15 17:11:01 +0530

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+const expect = require('expect');
+
+const {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        const from = 'test@example.com';
+        const text = 'Test message';
+        const message = generateMessage(from, text);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});
+    });
+});
